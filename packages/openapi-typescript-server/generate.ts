612cc7bf525f47464d78b4cff54f57202de24772
jasonblanchard <jas.blanchard@gmail.com>
2025-03-02 16:56:48 -0500

    args and stuff

---
+++ packages/openapi-typescript-server/generate.ts
@@ -0,0 +1,9 @@
+import type { OpenAPISpec } from "./openapiValidator";
+
+export default function generate(
+  spec: OpenAPISpec,
+  types: string,
+  outDir: string
+) {
+  console.log("TODO", spec, types, outDir);
+}
