e80d9ce2afd225a44b278bef52ca25933e3eb54d
jasonblanchard <jas.blanchard@gmail.com>
2025-03-02 16:34:18 -0500

    more structural stuff

--- packages/openapi-typescript-server/bin/index.ts
+++ packages/openapi-typescript-server/bin/index.ts
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-console.log("Chello");
+import { Command } from "commander";
+import fs from "fs";
+import { OpenAPISpec } from "./openapiValidator.ts";
+import yaml from "js-yaml";
+
+const program = new Command();
+
+program
+  .name("open-api-typescript-server")
+  .description("CLI to some Open API Stuff")
+  .version("0.0.0");
+
+program
+  .command("build")
+  .description("Output generated code")
+  .option("-s, --spec <file>", "OpenAPI spec file", "")
+  .option("-o, --out <dir>", "output directory", "")
+  .action(async (options) => {
+    const specS = fs.readFileSync(options.spec, "utf-8");
+
+    const specPojo = yaml.load(specS) as Map<string, any>;
+
+    const validateSpecResponse = OpenAPISpec.safeParse(specPojo);
+
+    if (!validateSpecResponse.success) {
+      console.error(validateSpecResponse.error.errors);
+      return;
+    }
+
+    console.log("TODO: Implement code generation");
+  });
+
+program.parse();
---
+++ packages/openapi-typescript-server/bin/openapiValidator.ts
@@ -0,0 +1,68 @@
+import z from "zod";
+
+// Allow Schema to recursively reference itself in properties
+const bastSchema = z.object({
+  $ref: z.string().optional(),
+  type: z.string().optional(), // Actually a set of known literals. Can probably do a discriminated union instead of making the types optional
+  required: z.array(z.string()).optional(),
+});
+
+export type Schema = z.infer<typeof bastSchema> & {
+  properties?: Record<string, Schema>;
+};
+
+const Schema: z.ZodType<Schema> = bastSchema.extend({
+  properties: z.lazy(() => z.record(Schema).optional()),
+});
+
+export const OpenAPISpec = z.object({
+  openapi: z.string(),
+  info: z.object({
+    title: z.string().optional(),
+    version: z.string().optional(),
+  }),
+  paths: z.record(
+    z.record(
+      z.object({
+        summary: z.string().optional(),
+        operationId: z.string(),
+        parameters: z
+          .array(
+            z.object({
+              name: z.string(),
+              in: z.string(),
+              required: z.boolean().optional(),
+              schema: z.object({
+                type: z.string(),
+              }),
+            })
+          )
+          .optional(),
+        requestBody: z
+          .object({
+            description: z.string().optional(),
+            content: z.record(
+              z.object({
+                schema: Schema,
+              })
+            ),
+          })
+          .optional(),
+        responses: z.record(
+          z.object({
+            description: z.string().optional(),
+            content: z
+              .record(
+                z.object({
+                  schema: Schema,
+                })
+              )
+              .optional(),
+          })
+        ),
+      })
+    )
+  ),
+});
+
+export type OpenAPISpec = z.infer<typeof OpenAPISpec>;
