812decaa473c4a526a7cac34806a43dcfa27c404
jasonblanchard <jas.blanchard@gmail.com>
2025-03-20 22:24:54 -0400

    fix formatting

--- packages/openapi-typescript-server/src/cli/generate.ts
+++ packages/openapi-typescript-server/src/cli/generate.ts
@@ -1,7 +1,11 @@
 import type { OpenAPISpec } from "../lib/schema";
 import { Project } from "ts-morph";
 
-export default function generate(spec: OpenAPISpec, types: string, outdir: string) {
+export default function generate(
+  spec: OpenAPISpec,
+  types: string,
+  outdir: string,
+) {
   const project = new Project();
 
   const sourceFile = project.createSourceFile(`${outdir}/server.ts`, "", {
@@ -131,7 +135,7 @@ export default function generate(spec: OpenAPISpec, types: string, outdir: strin
           args: ${args}<Req, Res>
           ) => ${result}`,
       };
-    }
+    },
   );
 
   const serverInterface = sourceFile.addInterface({
@@ -162,7 +166,7 @@ export default function generate(spec: OpenAPISpec, types: string, outdir: strin
           writer.writeLine(`path: "${path}",`);
           writer.writeLine(`handler: server.${operationId},`);
           writer.writeLine("},");
-        }
+        },
       );
 
       writer.writeLine("]");
@@ -176,7 +180,7 @@ export default function generate(spec: OpenAPISpec, types: string, outdir: strin
  * Do not make direct changes to the file.
  */
 
-  `
+  `,
   );
 
   sourceFile.formatText({
